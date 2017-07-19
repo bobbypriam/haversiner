41757a1905c115bd288154fd84c954967fa567fc
Bobby Priambodo <bobby.priambodo@gmail.com>
2017-07-19 20:13:16 +0700

    Name the parameters for readability

--- test/test.ml
+++ test/test.ml
@@ -9,15 +9,15 @@ let test () =
   print_endline ("Tolerating +- " ^ string_of_int (int_of_float (epsilon *. 1000.)) ^ " meters");
 
   (* Distance from BNA to LAX should roughly be 2886.45 km *)
-  let bna = coordinate_of_floats 36.12 (-86.67) in
-  let lax = coordinate_of_floats 33.94 (-118.4) in
+  let bna = coordinate_of_floats ~lat:36.12 ~long:(-86.67) in
+  let lax = coordinate_of_floats ~lat:33.94 ~long:(-118.4) in
   let result = calculate_distance bna lax in
   print_endline ("BNA-LAX: expected = 2886.45 km, actual = " ^ string_of_float result);
   assert (close_enough result 2886.45);
 
   (* Distance from Jakarta to Bandung should roughly be 114.78 km *)
-  let jak = coordinate_of_floats (-6.21462) 106.84513 in
-  let bdg = coordinate_of_floats (-6.90389) 107.61861 in
+  let jak = coordinate_of_floats ~lat:(-6.21462) ~long:106.84513 in
+  let bdg = coordinate_of_floats ~lat:(-6.90389) ~long:107.61861 in
   let result = calculate_distance jak bdg in
   print_endline ("JAK-BDG: expected = 114.78 km, actual = " ^ string_of_float result);
   assert (close_enough result 114.78)
