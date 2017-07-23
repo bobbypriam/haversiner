169d91e1080d0bf8c0be83ff23eb0b3d04705dd9
Bobby Priambodo <bobby.priambodo@gmail.com>
2017-07-23 11:32:51 +0700

    Improve test suite formatting

--- test/haversine_test.ml
+++ test/haversine_test.ml
@@ -6,25 +6,25 @@ let epsilon = 0.01 (* We tolerate difference of +- 10 meters *)
 let close_enough a b =
   abs_float (a -. b) < epsilon
 
-let suite = "Haversine" >:::
-[
-  "calculate_distance BNA-LAX" >:: (fun _ ->
-    (* Distance from BNA to LAX should roughly be 2886.45 km *)
-    let bna = coordinate_of_floats ~lat:36.12 ~long:(-86.67) in
-    let lax = coordinate_of_floats ~lat:33.94 ~long:(-118.4) in
+let suite =
+  "Haversine" >::: [
+    "calculate_distance BNA-LAX" >:: (fun _ ->
+      (* Distance from BNA to LAX should roughly be 2886.45 km *)
+      let bna = coordinate_of_floats ~lat:36.12 ~long:(-86.67) in
+      let lax = coordinate_of_floats ~lat:33.94 ~long:(-118.4) in
 
-    let result = calculate_distance bna lax in
+      let result = calculate_distance bna lax in
 
-    assert_equal ~cmp:close_enough result 2886.45
-  );
+      assert_equal ~cmp:close_enough result 2886.45
+    );
 
-  "calculate_distance JAK-BDG" >:: (fun _ ->
-    (* Distance from Jakarta to Bandung should roughly be 114.78 km *)
-    let jak = coordinate_of_floats ~lat:(-6.21462) ~long:106.84513 in
-    let bdg = coordinate_of_floats ~lat:(-6.90389) ~long:107.61861 in
+    "calculate_distance JAK-BDG" >:: (fun _ ->
+      (* Distance from Jakarta to Bandung should roughly be 114.78 km *)
+      let jak = coordinate_of_floats ~lat:(-6.21462) ~long:106.84513 in
+      let bdg = coordinate_of_floats ~lat:(-6.90389) ~long:107.61861 in
 
-    let result = calculate_distance jak bdg in
+      let result = calculate_distance jak bdg in
 
-    assert_equal ~cmp:close_enough result 114.78
-  )
-]
+      assert_equal ~cmp:close_enough result 114.78
+    );
+  ]
