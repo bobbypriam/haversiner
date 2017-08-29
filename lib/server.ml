f086a4aff5cee853c70a6c8a658d6bd52df7ed01
Bobby Priambodo <bobby.priambodo@gmail.com>
2017-08-29 21:37:12 +0700

    Reformat, add print on start

--- lib/server.ml
+++ lib/server.ml
@@ -8,15 +8,16 @@ let distance_handler req =
   req
   |> App.json_of_body_exn
   |> Lwt.map (fun json ->
-    let json_value = Ezjsonm.value json in
-    let (a, b) = Coordinate_parser.parse json_value in
-    let result = Haversine.calculate_distance a b in
-    `Json (json_of_result result) |> respond
-  )
+      let json_value = Ezjsonm.value json in
+      let (a, b) = Coordinate_parser.parse json_value in
+      let result = Haversine.calculate_distance a b in
+      `Json (json_of_result result) |> respond
+    )
 
 let distance_route = post "/" distance_handler
 
 let start () =
+  print_endline "Running server at port 3000.";
   App.empty
   |> distance_route
   |> App.cmd_name "haversiner"
